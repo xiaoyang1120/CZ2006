8ad3311b7fba471e3948e597e69616d93c0bca20
Zhang Xinyi <zh0031yi@e.ntu.edu.sg>
2020-03-19 09:57:20 +0800

    Added java backend

---
+++ JavaBackend/src/main/java/softwareGenius/api/DemoController.java
@@ -0,0 +1,42 @@
+package softwareGenius.api;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import softwareGenius.model.DemoModel;
+import softwareGenius.service.DemoModelService;
+
+import java.util.List;
+
+@RequestMapping("api/demo")
+@RestController
+public class DemoController {
+
+    private DemoModelService demoModelService;
+
+    @Autowired
+    public DemoController(DemoModelService demoModelService) {
+        this.demoModelService = demoModelService;
+    }
+
+
+    @GetMapping("/get/{id}")
+    public DemoModel get(@PathVariable Integer id){
+        return demoModelService.getOne(id);
+    }
+
+    @GetMapping("getAll")
+    public List<DemoModel> getAll(){
+        return demoModelService.getAll();
+    }
+
+    @PostMapping("/add")
+    public Boolean add(@RequestBody DemoModel user){
+        return demoModelService.add(user);
+    }
+
+    @GetMapping(path = "{msg}")
+    public String demoApi(@PathVariable("msg") String msg) {
+        return msg;
+    }
+
+}
