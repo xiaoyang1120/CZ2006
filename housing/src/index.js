3431a81ca9f11159e7ce6b9bd5ad3671491830c6
Ethan <ganx0005@e.ntu.edu.sg>
2020-03-19 12:57:42 +0800

    map

--- housing/src/index.js
+++ housing/src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SimpleMap from './map';
 // import Button from '@material-ui/core/Button';
 const element = <h1> Housing </h1>;
-const element2 = <button>This is a button which is useless</button>;
+const element2 = <SimpleMap />;
 ReactDOM.render(element, document.getElementById('root'));
 ReactDOM.render(element2, document.getElementById('root2'));
---
+++ housing/src/map.js
@@ -0,0 +1,30 @@
+import GoogleMapReact from 'google-map-react';
+import React, { Component } from 'react';
+const AnyReactComponent = ({ text }) => <div>{text}</div>;
+
+class SimpleMap extends Component {
+  static defaultProps = {
+    center: {
+      lat: 1.36,
+      lng: 103.84
+    },
+    zoom: 11.8
+  };
+
+  render() {
+    return (
+      // Important! Always set the container height explicitly
+      <div style={{ height: '100vh', width: '100%' }}>
+        <GoogleMapReact
+          bootstrapURLKeys={{ key: 'AIzaSyDENKvL3vnCR5J-5D_CROgv_7IS1Kk00WI' }}
+          defaultCenter={this.props.center}
+          defaultZoom={this.props.zoom}
+        >
+          <AnyReactComponent lat={1.36} lng={103.84} text='My Marker' />
+        </GoogleMapReact>
+      </div>
+    );
+  }
+}
+
+export default SimpleMap;
