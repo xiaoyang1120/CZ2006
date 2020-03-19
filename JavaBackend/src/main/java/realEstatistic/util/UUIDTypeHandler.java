0d462aba8a2cd8e37a8294a3ad25b6200a5a193b
Ethan <ganx0005@e.ntu.edu.sg>
2020-03-19 15:34:55 +0800

    UUID

---
+++ JavaBackend/src/main/java/realEstatistic/util/UUIDTypeHandler.java
@@ -0,0 +1,29 @@
+import java.sql.CallableStatement;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.UUID;
+ 
+import org.apache.ibatis.type.BaseTypeHandler;
+import org.apache.ibatis.type.JdbcType;
+ 
+public class UUIDTypeHandler extends BaseTypeHandler {
+ 
+	@Override
+	public Object getNullableResult(ResultSet rs, String columnName)
+			throws SQLException {
+		return UUID.fromString(rs.getString(columnName));
+	}
+ 
+	@Override
+	public Object getNullableResult(CallableStatement cs, int columnIndex)
+			throws SQLException {
+		return UUID.fromString((cs.getString(columnIndex)));
+	}
+ 
+	@Override
+	public void setNonNullParameter(PreparedStatement ps, int i,
+			Object parameter, JdbcType jdbcType) throws SQLException {
+		ps.setString(i, ((UUID) parameter).toString());
+	}
+}
