4f6eb927ac6920c937a452ba12c799237ff50368
Zhang Xinyi <zh0031yi@e.ntu.edu.sg>
2020-03-19 14:57:17 +0800

    fixed annotation

--- JavaBackend/src/main/java/realEstatistic/mapper/DistrictDao.java
+++ JavaBackend/src/main/java/realEstatistic/mapper/DistrictDao.java
@@ -1,10 +1,14 @@
 package realEstatistic.mapper;
 
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Component;
 import realEstatistic.model.District;
 
 import java.util.List;
 import java.util.UUID;
 
+@Mapper
+@Component
 public interface DistrictDao {
     District getDistrictById(UUID districtId);
     List<District> getAllDistrict();
