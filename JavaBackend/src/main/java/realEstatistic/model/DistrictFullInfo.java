ea5ee41e6c27ba6fc7501056c9aa3e0a3e6170a8
Ethan <ganx0005@e.ntu.edu.sg>
2020-03-22 10:58:57 +0800

    full info

--- JavaBackend/src/main/java/realEstatistic/model/DistrictFullInfo.java
+++ JavaBackend/src/main/java/realEstatistic/model/DistrictFullInfo.java
@@ -8,10 +8,35 @@ public class DistrictFullInfo extends DistrictInfo{
     private List<Float> districtRange;
     private String Name;
     private String Description;
+
     public DistrictFullInfo(UUID districtId, int numOfPrimary, int numOfSecondary, int numOfJc, int numOfMixed, int numOfPark, int numOfSupermarket, int numOfHawkerCentre, int numOfClinic, int numOfPremiumBus, int numOfEWaste, int numOfMRT, List<Float> districtRange, String Name, String Description) {
         super(districtId, numOfPrimary, numOfSecondary, numOfJc, numOfMixed, numOfPark, numOfSupermarket, numOfHawkerCentre, numOfClinic, numOfPremiumBus, numOfEWaste, numOfMRT);
         this.districtRange=districtRange;
         this.Name=Name;
         this.Description=Description;
+    }
+
+    public List<Float> getDistrictRange() {
+        return this.districtRange;
+    }
+
+    public String getName() {
+        return this.Name;
+    }
+
+    public String getDescription() {
+        return this.Description;
+    }
+
+    public void setDistrictRange(List<Float> districtRange) {
+        this.districtRange = districtRange;
+    }
+
+    public void setName(String name) {
+        this.Name = name;
+    }
+
+    public void setDescription(String description) {
+        this.Description = description;
     }
 }
