bfb924156f22855f0560a11d333cc1b431538bdb
Leon Xiao <42631166+xiaoyang1120@users.noreply.github.com>
2020-04-01 16:56:07 +0800

    resume redirect logic

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ import "./App.css";
 // import {CSSTransition, TransitionGroup} from "react-transition-group";
 
 import {
-  BrowserRouter as Router,
-  Switch,
-  Route
+    BrowserRouter as Router,
+    Switch,
+    Route, Redirect
 } from "react-router-dom";
 import LoginPage from "./Boundary/LoginPage";
 import PrimaryCriteriaMatching from "./Boundary/PrimaryCriteriaMatching";
@@ -15,106 +15,94 @@ import SecondaryCriteriaMatching from "./Boundary/SecondaryCriteriaMatching";
 import UploadHousePage from "./Boundary/UploadHousePage";
 import AreaListUI from "./Boundary/AreaListUI";
 import HouseList from "./Boundary/HouseList";
+
 class App extends React.Component {
-  constructor() {
-    super();
+    constructor() {
+        super();
 
-    this.state = {
-      loggedInStatus: sessionStorage.getItem("loggedInStatus"),
-      email: sessionStorage.getItem("email"),
-      uuid: sessionStorage.getItem("uuid")
-    };
+        this.state = {
+            loggedInStatus: sessionStorage.getItem("loggedInStatus"),
+            email: sessionStorage.getItem("email"),
+            uuid: sessionStorage.getItem("uuid")
+        };
 
-    this.handleLogin = this.handleLogin.bind(this);
-    this.handleLogout = this.handleLogout.bind(this);
-  }
+        this.handleLogin = this.handleLogin.bind(this);
+        this.handleLogout = this.handleLogout.bind(this);
+    }
 
-  handleLogout() {
-    this.setState({
-      loggedInStatus: "NOT_LOGGED_IN",
-      email: null
-    });
-    sessionStorage.setItem("loggedInStatus", "NOT_LOGGED_IN");
-    sessionStorage.setItem("email", null);
-    sessionStorage.setItem("uuid", null);
-  }
+    handleLogout() {
+        this.setState({
+            loggedInStatus: "NOT_LOGGED_IN",
+            email: null
+        });
+        sessionStorage.setItem("loggedInStatus", "NOT_LOGGED_IN");
+        sessionStorage.setItem("email", null);
+        sessionStorage.setItem("uuid", null);
+    }
 
-  handleLogin(email, uuid) {
-    this.setState({
-      loggedInStatus: "LOGGED_IN",
-      email: email
-    });
+    handleLogin(email, uuid) {
+        this.setState({
+            loggedInStatus: "LOGGED_IN",
+            email: email
+        });
 
-    sessionStorage.setItem("loggedInStatus", "LOGGED_IN");
-    sessionStorage.setItem("email", email);
-    sessionStorage.setItem("uuid", uuid);
-  }
+        sessionStorage.setItem("loggedInStatus", "LOGGED_IN");
+        sessionStorage.setItem("email", email);
+        sessionStorage.setItem("uuid", uuid);
+    }
 
-  render() {
-    const loginStatus = this.state.loggedInStatus;
-    return (
-      <Router>
-        <div>
-          <Switch>
-            <Route path={"/"} exact component={HomePage} />
-
-            <Route
-              exact
-              path="/login"
-              render={props => (
-                <LoginPage
-                  {...props}
-                  handleLogin={this.handleLogin}
-                  loggedInStatus={loginStatus}
-                />
-              )}
-            />
-            <Route
-              path={"/profile"}
-              render={props => (
-                <ProfilePage {...props} handleLogout={this.handleLogout} />
-              )}
-            />
-            <Route
-              path={"/criteria"}
-              exact
-              component={PrimaryCriteriaMatching}
-            />
-            <Route
-              path={"/criteria_"}
-              exact
-              component={SecondaryCriteriaMatching}
-            />
-            <Route path={"/arealist"} exact component={AreaListUI} />
-            <Route path={"/house@area"} exact component={HouseList} />
-
-            <Route path={"/upload"} exact component={UploadHousePage} />
-
-            {/*<Route exact path="/login"*/}
-            {/*       render={props => (*/}
-            {/*           <LoginPage*/}
-            {/*               {...props}*/}
-            {/*               handleLogin={this.handleLogin}*/}
-            {/*               loggedInStatus={loginStatus}*/}
-            {/*           />*/}
-            {/*       )}/>*/}
-            {/*<Route path={loginStatus ? "/profile" : "/login"}*/}
-            {/*       render={*/}
-            {/*           props => (*/}
-            {/*               <ProfilePage*/}
-            {/*                   {...props}*/}
-            {/*                   handleLogout={this.handleLogout}*/}
-            {/*               />*/}
-            {/*           )*/}
-            {/*       }/>*/}
-            {/*<Route path={loginStatus ? "/criteria" : "/login"}*/}
-            {/*       exact component={CriteriaMatching}/>*/}
-            {/*<Redirect from="/*" to="/login"/>*/}
-          </Switch>
-        </div>
-      </Router>
-    );
-  }
+    render() {
+        const loginStatus = this.state.loggedInStatus;
+        return (
+            <Router>
+                <div>
+                        {
+                            loginStatus ?
+                                <Switch>
+                                    <Route path={"/"} exact component={HomePage}/>
+                                    <Route exact path="/login"
+                                           render={props => (
+                                               <LoginPage
+                                                   {...props}
+                                                   handleLogin={this.handleLogin}
+                                                   loggedInStatus={loginStatus}
+                                               />
+                                           )}/>
+                                    <Route path="/profile"
+                                           render={
+                                               props => (
+                                                   <ProfilePage
+                                                       {...props}
+                                                       handleLogout={this.handleLogout}
+                                                   />
+                                               )
+                                           }/>
+                                    <Route path="/criteria" exact component={PrimaryCriteriaMatching}/>
+                                    <Route path="/criteria_" exact component={SecondaryCriteriaMatching}
+                                    />
+                                    <Route path="/arealist" exact component={AreaListUI}/>
+                                    <Route path="/house@area" exact component={HouseList}/>
+                                    <Route path="/upload" exact component={UploadHousePage}/>
+                                    <Redirect from="/*" to="/"/>
+                                </Switch>
+                                :
+                                <Switch>
+                                    <Route path={"/"} exact component={HomePage}/>
+                                    <Route exact path="/login"
+                                           render={props => (
+                                               <LoginPage
+                                                   {...props}
+                                                   handleLogin={this.handleLogin}
+                                                   loggedInStatus={loginStatus}
+                                               />
+                                           )}/>
+                                    <Redirect from="/*" to="/login"></Redirect>
+                                </Switch>
+                        }
+                </div>
+            </Router>
+        );
+    }
 }
 
 export default App;
