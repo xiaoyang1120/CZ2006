60dd44cb1f877b14f7df5586603d171286f8de8b
Leon Xiao <42631166+xiaoyang1120@users.noreply.github.com>
2020-04-02 14:52:18 +0800

    add db

--- JavaBackend/src/main/java/realEstatistic/config/CronTime.java
+++ JavaBackend/src/main/java/realEstatistic/config/CronTime.java
@@ -1,6 +1,6 @@
 package realEstatistic.config;
 
 public class CronTime {
-    public static final String fetchTime = "0 35 22 * * *";
-    public static final String updateTime = "0 36 22 * * *";
+    public static final String fetchTime = "0 0 0 * * *";
+    public static final String updateTime = "0 1 0 * * *";
 }
